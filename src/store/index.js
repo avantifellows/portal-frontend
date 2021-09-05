f41b6b31a06fa290eb682b9e97e9a558212bb945
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2021-09-05 17:26:33 +0530

    deleted unused packages

--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,8 @@
 import { createStore } from "vuex";
-import createPersistedState from "vuex-persistedstate";
-import SecureLS from "secure-ls";
 import auth from "./modules/auth";
-
-var localStorage = new SecureLS({ isCompression: false });
 
 export default createStore({
     plugins: [
-        createPersistedState({
-        storage: {
-            getItem: (key) => {
-            try {
-                return localStorage.get(key);
-            } catch (error) {
-                console.log(error);
-            }
-            },
-            setItem: (key, value) => localStorage.set(key, value),
-            removeItem: (key) => localStorage.remove(key),
-        },
-        }),
     ],
     modules: {
         auth

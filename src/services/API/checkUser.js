107a343d753d93805117802b9c32bc0611a657c2
Krrupa <25496389+sudheshna-donthineni@users.noreply.github.com>
2021-09-06 13:51:44 +0530

    Update checkUser.js

--- src/services/API/checkUser.js
+++ src/services/API/checkUser.js
@@ -1,5 +1,5 @@
-import {client} from "./rootClient.js"
-import { checkUserEndpoint } from "./endpoints.js";
+import {client} from "@/API/rootClient.js"
+import { checkUserEndpoint } from "@/API/endpoints.js";
 
 export default {
     //checks if the user exists in the database or not

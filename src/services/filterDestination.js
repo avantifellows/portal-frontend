bc45bba74a99f40b02fb87390338b8345547ebfc
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2021-09-17 17:04:00 +0530

    modified redirectToDestination

--- src/services/filterDestination.js
+++ src/services/filterDestination.js
@@ -1,12 +1,26 @@
-import { redirectToPlio, redirectToMeet } from "@/services/API/redirectToDestination.js";
-
 export function filterDestination(redirectTo, purposeParams, userID, redirectID){
+    var redirectURL = "";
+    var fullurl = "";
+
     switch(purposeParams){
         case 'plio':
-            redirectToPlio( userID, redirectID);
+            //this method constructs the URL based on the redirectTo param
+            redirectURL = process.env.VUE_APP_BASE_URL_PLIO;
+            var url = new URL(redirectURL + redirectID); //adds plioID to the base plio link
+            //adds params; api key and student SRN
+            var queryparams = new URLSearchParams({
+                                api_key: process.env.VUE_APP_AF_API_KEY,
+                                unique_id: userID,
+                                });
+            fullurl = url + "?" + queryparams;
             break;
+
         case 'liveclass':
-            redirectToMeet(redirectID)
+            //this method constructs the URL based on the redirectTo param
+            redirectURL = process.env.VUE_APP_BASE_URL_MEET;
+            fullurl = new URL(redirectURL + redirectID); //adds meetID to the base plio link
             break;
         }
+        
+    window.open(fullurl, "_self");
 }

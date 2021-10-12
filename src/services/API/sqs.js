cf01d79d8932327b57909415a71c7251fc059320
Krrupa <25496389+sudheshna-donthineni@users.noreply.github.com>
2021-10-12 09:49:15 +0530

    Update sqs.js

--- src/services/API/sqs.js
+++ src/services/API/sqs.js
@@ -14,7 +14,7 @@ export async function sendSQSMessage(purpose, purposeParams, redirectTo, redirec
 
     const messageBody = [
       {
-        dateTime: Date.now(),
+        dateTime: new Date(),
         purpose: {
           type: purpose,
           subType: purposeParams,

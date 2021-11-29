cb97ae4f2553429a06f5a6a9cfa78f7ad5c389a9
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2021-11-29 14:33:44 +0530

    removed store

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router";
-import store from "./store";
 import './index.css';
 import InlineSvg from "vue-inline-svg";
 
-const app = createApp(App).component('inline-svg', InlineSvg).use(store).use(router);
+const app = createApp(App).component('inline-svg', InlineSvg).use(router);
 
 
 app.mount('#app');

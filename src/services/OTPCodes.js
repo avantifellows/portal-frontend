e10516726abb856dbdcc4432e1178085376c6d99
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2021-12-24 10:52:22 +0530

    added language

--- src/services/OTPCodes.js
+++ src/services/OTPCodes.js
@@ -1,12 +1,12 @@
 export const mapVerifyStatusCodeToMessage = {
-    311: "This OTP does not exist",
-    309: "You have exceeded maximum number of attempts. Please try after sometime.",
-    301: "OTP token is expired.",
-    310: "This OTP is incorrect.",
-    308: "You are re-trying too early. Please wait for sometime.",
+    311: "This OTP does not exist / यह OTP मौजूद नहीं है",
+    309: "You have exceeded maximum number of attempts. Please try after sometime / आप प्रयासों की अधिकतम संख्या को पार कर चुके हैं। कृपया कुछ देर बाद प्रयास करें",
+    301: "OTP token is expired / OTP टोकन की समय सीमा समाप्त हो गई है",
+    310: "This OTP is incorrect / यह OTP गलत है",
+    308: "You are re-trying too early. Please wait for sometime / आप बहुत जल्दी पुनः प्रयास कर रहे हैं। कृपया कुछ देर प्रतीक्षा करें ",
   }
 
   export function mapSendStatusCodeToMessage(code) {
-    code == "200" ? "OTP has been sent!" : "Oh no!"
+    return code == 200 ? "OTP has been sent! / OTP भेज दिया गया है!" : "Oh no!"
 
   }

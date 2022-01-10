86652e630cb731cc4ddd9e165e46a20a71fe3321
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2022-01-10 15:11:37 +0530

    added staging function

--- src/services/API/endpoints.js
+++ src/services/API/endpoints.js
@@ -1,4 +1,4 @@
-export const checkUserEndpoint = "/checkForUser";
+export const checkUserEndpoint = process.env.VUE_APP_CHECK_USER_ENDPOINT;
 export const getGroupDataEndpoint = "/getGroupData";
 export const sendOTPEndpoint = "/sendotp";
 export const verifyOTPEndpoint = "/verifyotp";

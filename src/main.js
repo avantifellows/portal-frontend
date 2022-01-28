fd60333f524bbb7bd65b27eed86217dfc39daa19
Vaibhav Rathore <vaibhav@coloredcow.com>
2022-01-28 17:42:26 +0530

    pre-commit fix

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import "./index.css";
 
 const app = createApp(App).component("inline-svg", InlineSvg).use(router);
 
-if (['staging', 'production'].includes(process.env.NODE_ENV)) {
+if (["staging", "production"].includes(process.env.NODE_ENV)) {
   Sentry.init({
     app,
     dsn: process.env.VUE_APP_SENTRY_DSN,

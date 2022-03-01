2556bdb38e8b5fe445a56a97a3fc44badbf79c49
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2022-03-01 14:39:55 +0530

    added staging url

--- src/services/API/rootClient.js
+++ src/services/API/rootClient.js
@@ -1,10 +1,5 @@
 import axios from "axios";
 
-// const headers = {
-//   Accept: "application/json",
-//   "Content-Type": "application/json",
-// };
-
 /** the base URL is the deployed cloud function URL */
 export const client = axios.create({
   baseURL: process.env.VUE_APP_CLOUD_FUNCTIONS_BASE_URL,

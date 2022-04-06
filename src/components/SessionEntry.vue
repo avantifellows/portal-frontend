25064bf8275b1eda8cab04a2a8816d0635395e8d
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2022-04-06 21:46:16 +0530

    session component

---
+++ src/components/SessionEntry.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+
+export default {
+  name: "SessionEntry",
+     props: {
+    sessionData: Object,
+     },
+     computed: {
+
+     },
+     async created() {
+
+   }
+};
+</script>

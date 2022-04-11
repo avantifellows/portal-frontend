5be5fe46899f5210fa0707c7e76d5180a8717911
Krrupa <25496389+sudheshna-donthineni@users.noreply.github.com>
2022-04-11 12:50:18 +0530

    Update rootClient.js

--- src/services/API/rootClient.js
+++ src/services/API/rootClient.js
@@ -5,7 +5,6 @@ export const client = axios.create({
   baseURL: import.meta.env.VITE_APP_CLOUD_FUNCTIONS_BASE_URL,
 });
 
-console.log(import.meta.env.VITE_APP_CLOUD_FUNCTIONS_BASE_URL)
 /** the base URL is the OTP service URL */
 export const OTPClient = axios.create({
   baseURL: import.meta.env.VITE_APP_OTP_SERVICE,

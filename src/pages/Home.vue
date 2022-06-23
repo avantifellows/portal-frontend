845b340cce52d57417f1b61be9ddfab6e0cb3692
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2022-06-23 13:39:04 +0530

    time guards calculated on server

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,97 +1,91 @@
 <template>
-<div v-if="isLoading">
-<div class="flex m-auto h-screen w-full h-full">
+  <div v-if="isLoading">
+    <div class="flex m-auto h-screen w-full h-full">
       <inline-svg
         class="text-black text-4xl m-auto animate-spin h-20 w-20"
         :src="loadingSpinnerSvg"
       />
     </div>
-    </div>
+  </div>
   <!-- NoClassMessage component -->
   <div v-if="!sessionEnabled">
     <NoClassMessage />
   </div>
   <div v-else>
     <!-- Entry component -->
-    <div v-if="isAuthTypeID && doesGroupExist">
+    <div v-if="isAuthTypeID && doesGroupExist && sessionEnabled">
       <Entry
-        :redirectTo="redirectTo"
-        :redirectID="redirectID"
-        :purpose="purpose"
-        :purposeParams="purposeParams"
+        :redirectTo="getRedirectTo"
+        :redirectID="getRedirectID"
+        :purpose="getPurpose"
+        :purposeParams="getPurposeParams"
         :groupData="groupData"
-        :group="group"
+        :group="getGroup"
         :authType="authType"
+        :sessionId="sessionId"
       />
     </div>
     <!-- OTP component -->
-    <div v-else-if="isAuthTypeOTP && doesGroupExist">
+    <div v-else-if="isAuthTypeOTP && doesGroupExist && sessionEnabled">
       <OTP
-        :redirectTo="redirectTo"
-        :redirectID="redirectID"
-        :purpose="purpose"
-        :purposeParams="purposeParams"
+        :redirectTo="getRedirectTo"
+        :redirectID="getRedirectID"
+        :purpose="getPurpose"
+        :purposeParams="getPurposeParams"
         :groupData="groupData"
-        :group="group"
+        :group="getGroup"
         :authType="authType"
-      />
-    </div>
-    <!-- Session component -->
-    <div v-else-if="sessionData">
-      <SessionEntry
-        :sessionData="sessionData"
+        :sessionId="sessionId"
       />
     </div>
   </div>
 </template>
 
 <script>
-import Entry from '@/components/Entry.vue';
-import OTP from '@/components/OTP.vue';
-import SessionEntry from '@/components/SessionEntry.vue';
-import groupAPIService from '@/services/API/groupData.js';
-import sessionAPIService from '@/services/API/sessionData.js';
-import NoClassMessage from '@/components/NoClassMessage.vue';
-import useAssets from '@/assets/assets.js'
+import Entry from "@/components/Entry.vue";
+import OTP from "@/components/OTP.vue";
+import groupAPIService from "@/services/API/groupData.js";
+import sessionAPIService from "@/services/API/sessionData.js";
+import NoClassMessage from "@/components/NoClassMessage.vue";
+import useAssets from "@/assets/assets.js";
 
 const assets = useAssets();
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     Entry,
     OTP,
-    SessionEntry,
     NoClassMessage,
   },
   props: {
     /** The resource we are redirecting to. Eg. redirectTo = plio tells us that we are redirecting to a plio. */
     redirectTo: {
-      default: '',
+      default: "",
       type: String,
     },
     /** ID of the resource. Eg. the plioID */
     redirectID: {
-      default: '',
+      default: "",
       type: String,
     },
     /** General category of why the data is being captured. Eg: attendance */
     purpose: {
-      default: '',
+      default: "",
       type: String,
     },
     /** Subcategory of the purpose. Eg: plio -> means the attendance is for a plio link */
     purposeParams: {
-      default: '',
+      default: "",
       type: String,
     },
     /** The group the user falls under. Eg: HaryanaStudents, DelhiStudents */
     group: {
-      default: 'HaryanaStudents',
+      default: "HaryanaStudents",
       type: String,
     },
     /** The authentication method used by the user */
     authType: {
-      default: 'ID',
+      default: "ID",
       type: String,
     },
     /** ID of session */
@@ -112,25 +106,52 @@ export default {
   computed: {
     /** Whether authentication method chosen is an ID entry */
     isAuthTypeID() {
-      return this.authType == 'ID';
+      return this.authType == "ID";
     },
     /** Whether authentication method chosen is OTP */
     isAuthTypeOTP() {
-      return this.authType == 'OTP';
+      return this.authType == "OTP";
     },
     doesGroupExist() {
       return this.groupData;
     },
+    /** Retrieves destination platform */
+    getRedirectTo() {
+      return this.redirectTo == "" ? this.sessionData.redirectPlatform : this.redirectTo;
+    },
+
+    /** Retrieves destination ID */
+    getRedirectID() {
+      return this.redirectID == ""
+        ? this.sessionData.redirectPlatformParams.id
+        : this.redirectID;
+    },
+
+    /** Retrieves group name */
+    getGroup() {
+      return this.group == "" ? this.sessionData.group : this.group;
+    },
+
+    /** Returns the purpose value stored in session data */
+    getPurpose() {
+      return this.purpose == "" ? this.sessionData.purpose : this.purpose;
+    },
+
+    /** Returns the purpose params stored in session data */
+    getPurposeParams() {
+      return this.purposeParams == ""
+        ? this.sessionData.purposeParams
+        : this.purposeParams;
+    },
   },
   async created() {
     /** If sessionId exists in route, then retrieve session details. Otherwise, fallback to using group data. */
-    if (this.sessionId == null) {
-      this.groupData = await groupAPIService.getGroupData(this.group);
-    } else {
+    if (this.sessionId != null) {
       this.sessionData = await sessionAPIService.getSessionData(this.sessionId);
-      this.sessionEnabled = (this.sessionData.enabled == 1);
+      this.sessionEnabled = this.sessionData.sessionActive;
     }
-    this.isLoading = false
+    this.groupData = await groupAPIService.getGroupData(this.group);
+    this.isLoading = false;
   },
 };
 </script>

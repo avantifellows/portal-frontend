7b07c2e4f0ba01e323bad135fe5a14df370b8b62
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2022-07-11 15:54:18 +0530

    added e2e testing

---
+++ cypress/e2e/end-to-end.cy.js
@@ -0,0 +1,75 @@
+import sessionIds from "../fixtures/sessionIds.json";
+
+let sessionOutput;
+
+describe("Display screen", () => {
+  sessionIds.forEach((sessionId) => {
+    beforeEach(() => {
+      cy.request({
+        method: "POST",
+        url: Cypress.env("backend") + "getSessionData",
+        body: JSON.stringify({
+          sessionId: sessionId,
+        }),
+        headers: {
+          "content-type": "application/x-www-form-urlencoded",
+        },
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.sessionActive);
+
+        sessionOutput = response.body.sessionActive;
+
+        if (response.body.sessionActive) {
+          cy.request({
+            method: "POST",
+            url: Cypress.env("backend") + "getGroupData",
+            body: JSON.stringify({
+              group: response.body.group,
+            }),
+            headers: {
+              "content-type": "application/x-www-form-urlencoded",
+            },
+          }).then((response) => {
+            expect(response.status).to.eq(200);
+          });
+        }
+      });
+    });
+
+    it("visits each URL", () => {
+      cy.visit(
+        "https://staging-auth.avantifellows.org/?sessionId=" + sessionId
+      );
+      if (!sessionOutput) {
+        cy.contains("p", "अभी आपकी कोई क्लास नहीं है|").should("be.visible");
+      } else {
+        cy.contains(
+          "p",
+          "Enter your Student ID / अपना Student ID दर्ज करें"
+        ).should("be.visible");
+      }
+    });
+
+    it("allows user to enter", () => {
+      cy.get("input").type("20180027508");
+
+      cy.request({
+        method: "POST",
+        url: Cypress.env("backend") + "checkForUser",
+        body: JSON.stringify({
+          userID: "20180027508",
+          collectionName: "DelhiStudents",
+          columnName: "student_id",
+        }),
+        headers: {
+          "content-type": "application/x-www-form-urlencoded",
+        },
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.be.true;
+      });
+      cy.contains("SUBMIT / जमा करें").click();
+    });
+  });
+});

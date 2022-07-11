24613d5bca036e091ba617b766110a426d6a1f13
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2022-07-11 19:56:04 +0530

    mock data

--- cypress/e2e/end-to-end.cy.js
+++ cypress/e2e/end-to-end.cy.js
@@ -1,17 +1,17 @@
-import sessionIds from "../fixtures/sessionIds.json";
+import mockData from "../fixtures/mockData.json";
 
 let sessionOutput;
 
 /** The whole workflow runs for each session given in the fixture. */
 describe("Display screen", () => {
-  sessionIds.forEach((sessionId) => {
+  mockData.forEach((data) => {
     // before each test, session details are retrieved and if session is active, group details are retrieved.
     beforeEach(() => {
       cy.request({
         method: "POST",
         url: Cypress.env("backend") + "getSessionData",
         body: JSON.stringify({
-          sessionId: sessionId,
+          sessionId: data.sessionId,
         }),
         headers: {
           "content-type": "application/x-www-form-urlencoded",
@@ -42,28 +42,25 @@ describe("Display screen", () => {
     // visited the URL with each session ID and asserts the displayed screen.
     it("visits each URL", () => {
       cy.visit(
-        "https://staging-auth.avantifellows.org/?sessionId=" + sessionId
+        "https://staging-auth.avantifellows.org/?sessionId=" + data.sessionId
       );
       if (!sessionOutput) {
-        cy.contains("p", "अभी आपकी कोई क्लास नहीं है|").should("be.visible");
+        cy.contains('[data-cy="noClassMessage"]').should("be.visible");
       } else {
-        cy.contains(
-          "p",
-          "Enter your Student ID / अपना Student ID दर्ज करें"
-        ).should("be.visible");
+        cy.contains('[data-cy="displayTitle"]').should("be.visible");
       }
     });
 
     it("allows user to enter", () => {
-      cy.get("input").type("20180027508");
+      cy.get('[data-cy="inputBox"]').type(data.userID);
 
       cy.request({
         method: "POST",
         url: Cypress.env("backend") + "checkForUser",
         body: JSON.stringify({
-          userID: "20180027508",
-          collectionName: "DelhiStudents",
-          columnName: "student_id",
+          userID: data.userID,
+          collectionName: data.collectionName,
+          columnName: data.columnName,
         }),
         headers: {
           "content-type": "application/x-www-form-urlencoded",
@@ -72,7 +69,7 @@ describe("Display screen", () => {
         expect(response.status).to.eq(200);
         expect(response.body).to.be.true;
       });
-      cy.contains("SUBMIT / जमा करें").click();
+      cy.contains('[data-cy="submitButton"]').click();
     });
   });
 });

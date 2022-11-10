c59eef5d3ce1a30a98ca424d4328e3ccf5f43ac2
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2022-11-10 10:40:56 +0530

    sign-up component

---
+++ src/components/Signup.vue
@@ -0,0 +1,256 @@
+<template>
+  <div class="h-full">
+    <!-- Header -->
+    <div class="flex w-full justify-center bg-white p-4">
+      <!-- <img src="../assets/images/jnvEnableLogo.png" /> -->
+    </div>
+    <div class="flex flex-col items-center justify-center p-10">
+      <p
+        class="text-xl lg:text-2xl xl:text-3xl mx-auto font-bold md:w-3/4 text-center mb-5"
+      >
+        PORTAL - SIGN UP
+      </p>
+
+      <FormKit type="form" :config="{ validationVisibility: 'submit' }">
+        <FormKit
+          type="text"
+          label="*Name"
+          validation="required|alpha_spaces|length:3,40"
+          v-model="studentName"
+          validation-visibility="live"
+          name="name"
+          help="Enter your full name. Example: Karn Mathur"
+        />
+        <FormKit
+          type="select"
+          label="*Gender"
+          placeholder="Select your gender"
+          :options="['Male', 'Female', 'Other']"
+          validation="required"
+          name="gender"
+          help="Select your gender."
+        />
+        <FormKit type="group" v-model="dateOfBirth" name="dob">
+          <div class="flex flex-row space-x-5">
+            <FormKit
+              type="select"
+              label="*Month"
+              name="month"
+              v-model="month"
+              placeholder="Month"
+              :options="monthList"
+              validation="required"
+              help="Select your Date of Birth."
+            />
+            <FormKit
+              type="select"
+              name="day"
+              v-model="day"
+              label="*Day"
+              placeholder="Day"
+              :options="dayList"
+              validation="required"
+            />
+            <FormKit
+              type="select"
+              name="year"
+              v-model="year"
+              label="*Year"
+              placeholder="Year"
+              :options="yearList"
+              validation="required"
+            />
+          </div>
+        </FormKit>
+        <FormKit
+          type="text"
+          label="*Father's Name"
+          validation="required|alpha_spaces|length:3,40"
+          name="father_name"
+          validation-visibility="live"
+          help="Enter your father's full name."
+        />
+        <FormKit
+          type="text"
+          label="*Mother's Name"
+          validation="required|alpha_spaces|length:3,40"
+          name="mother_name"
+          validation-visibility="live"
+          help="Enter your mother's full name."
+        />
+        <FormKit
+          type="select"
+          label="*Category"
+          placeholder="Select your category"
+          :options="['General', 'General-EWS', 'OBC', 'SC', 'ST']"
+          validation="required"
+          name="category"
+          help="Select your category. Example: OBC"
+        />
+        <FormKit
+          type="select"
+          label="*Physically Handicapped"
+          placeholder="Physically handicapped?"
+          :options="['Yes', 'No']"
+          validation="required"
+          name="physically_handicapped"
+          help="Please select Yes if you are physically handicapped"
+        />
+        <FormKit
+          type="select"
+          label="*Region"
+          placeholder="Select your region"
+          :options="[
+            'Bhopal',
+            'Chandigarh',
+            'Hyderabad',
+            'Jaipur',
+            'Lucknow',
+            'Patna',
+            'Pune',
+            'Shillong',
+          ]"
+          validation="required"
+          name="region"
+          v-model="region"
+          help="Please select your JNV's region. Example: Shillong"
+        />
+        <FormKit
+          type="select"
+          label="*State"
+          placeholder="Select your state"
+          :options="stateList || []"
+          validation="required"
+          v-model="stateName"
+          name="state"
+          help="Please select your JNV's state. Example: Sikkim"
+        />
+        <FormKit
+          type="text"
+          label="*City/Town"
+          validation="required|alpha_spaces|length:3,30"
+          validation-visibility="live"
+          name="city"
+          help="Enter your city's name. Example: Hyderabad"
+        />
+        <FormKit
+          type="select"
+          label="*School Name"
+          placeholder="Select your JNV's name"
+          :options="jnvList || []"
+          validation="required"
+          name="school"
+          help="Please select your JNV's name. Example: JNV Bidar"
+        />
+        <FormKit
+          type="select"
+          label="*Course"
+          placeholder="Which course?"
+          :options="['JEE', 'NEET']"
+          validation="required"
+          v-model="course"
+          name="course"
+          help="Please select your course: JEE or NEET"
+        />
+        <FormKit
+          type="select"
+          label="*Stream"
+          placeholder="Select your stream"
+          :options="streamList"
+          validation="required"
+          name="stream"
+          help="Please select your stream. Example: PCM (Physics, Chemistry, Math)"
+        />
+        <FormKit
+          type="select"
+          label="*Grade"
+          placeholder="Select your grade"
+          :options="['11', '12']"
+          v-model="grade"
+          validation="required"
+          name="grade"
+          help="Select 12 if you are in 12th grade/class"
+        />
+        <FormKit
+          type="email"
+          label="Email Address"
+          validation="email"
+          validation-visibility="live"
+          name="email"
+          help="Enter a valid email address if you have one. Example: karn.mathur@gmail.com"
+        />
+
+        <FormKit
+          type="tel"
+          label="*Phone Number"
+          validation="required|matches:/^[0-9]{10}$/"
+          validation-visibility="live"
+          v-model="phoneNumber"
+          name="number"
+          help="Please enter a valid mobile number. Example: 9848022335"
+        />
+
+        <FormKit
+          type="tel"
+          label="*Family Income per Annum"
+          validation="required|number|length:1,10"
+          validation-visibility="live"
+          name="family_income"
+          help="Please enter your family income per annum (year) in digits. Example: 100000"
+        />
+      </FormKit>
+    </div>
+  </div>
+</template>
+
+<script>
+import { jnvState, regionState } from "@/services/regionToJnvMapping.js";
+
+export default {
+  name: "Signup",
+  data() {
+    return {
+      region: "",
+      course: "",
+      grade: "",
+      stateName: "",
+      studentName: "",
+      phoneNumber: "",
+      dateOfBirth: "",
+      month: "",
+      day: "",
+      year: "",
+      monthList: Array.from({ length: 12 }, (_, i) => i + 1),
+      dayList: Array.from({ length: 31 }, (_, i) => i + 1),
+      yearList: Array.from({ length: 30 }, (_, i) => i + 1989).reverse(),
+    };
+  },
+
+  computed: {
+    stateList() {
+      return regionState[this.region];
+    },
+    jnvList() {
+      return jnvState[this.stateName];
+    },
+    streamList() {
+      if (this.course == "JEE") {
+        return ["PCM", "PCMB"];
+      } else {
+        return ["PCB", "PCMB"];
+      }
+    },
+  },
+  // methods: {
+  //   async submitForm() {
+  //     // check if good submission
+  //     await FormDataAPI.submitFormData(formData);
+  //     router.push({
+  //       name: "Player",
+  //       params: { quizId: quizId.value },
+  //       query: { userId: userId.value, apiKey: "6qOO8UdF1EGxLgzwIbQN" },
+  //     });
+  //   },
+  // },
+};
+</script>

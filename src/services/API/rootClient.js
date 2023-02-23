1736a26493cde9f42c45a7a0a2bb0b08d2765949
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2023-02-23 11:48:04 +0530

    new backend

--- src/services/API/rootClient.js
+++ src/services/API/rootClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 /** the base URL is the deployed cloud function URL */
 export const client = axios.create({
-  baseURL: import.meta.env.VITE_APP_CLOUD_FUNCTIONS_BASE_URL,
+  baseURL: import.meta.env.VITE_APP_PORTAL_BACKEND,
 });
 
 /** the base URL is the OTP service URL */

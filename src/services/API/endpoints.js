2f041838bb4167b42868b31b280b8ee11ecaf882
Krrupa <25496389+sudheshna-donthineni@users.noreply.github.com>
2023-04-04 10:06:10 +0530

    Update endpoints.js

--- src/services/API/endpoints.js
+++ src/services/API/endpoints.js
@@ -1,7 +1,5 @@
-export const checkUserEndpoint = import.meta.env.VITE_APP_CHECK_USER_ENDPOINT;
 export const sendOTPEndpoint = "/sendotp";
 export const verifyOTPEndpoint = "/verifyotp";
-export const checkBirthdateEndpoint = "/checkBirthdate";
 export const studentSignupEndpoint = "/studentSignup";
 export const userSignupEndpoint = "/userSignup";
 export const getGroupEndpoint = "/group";

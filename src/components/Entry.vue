b663dfb30ed35ead09088771d2af395ce0736b86
Krrupa <25496389+sudheshna-donthineni@users.noreply.github.com>
2023-05-04 13:04:59 +0530

    Update Entry.vue

--- src/components/Entry.vue
+++ src/components/Entry.vue
@@ -525,7 +525,7 @@ export default {
      */
     async authenticateID(userID) {
       this.isLoading = true;
-
+      console.log("why aren't you working!");
       let userValidationResponse = await validateID(
         userID,
         this.authType,

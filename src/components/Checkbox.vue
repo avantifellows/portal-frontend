733cac97808c115915921d17fff8656d3a070e79
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2023-05-30 12:55:54 +0530

    Added checkbox

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="flex flex-col justify-center">
+    <div class="flex flex-row justify-center">
+      <FormKit
+        type="checkbox"
+        :label="label"
+        :help="helpText"
+        :name="dbKey"
+        v-model="value"
+        :validation="[isRequired ? ['required'] : []]"
+        validation-visibility="dirty"
+      />
+    </div>
+    <span
+      v-if="isInvalidEntryMessageShown"
+      class="mx-auto text-red-700 text-base mb-1"
+      >{{ invalidEntryMessage }}</span
+    >
+  </div>
+</template>
+<script>
+export default {
+  name: "Checkbox",
+  emits: ["update"],
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    isRequired: {
+      type: Boolean,
+      default: false,
+    },
+    helpText: {
+      type: String,
+      default: "",
+    },
+    dbKey: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      value: "",
+      invalidEntryMessage: "",
+    };
+  },
+  watch: {
+    value(newValue, oldValue) {
+      this.$emit("update", this.value, this.dbKey);
+    },
+  },
+  computed: {
+    /** Returns if message for an invalid text entry needs to be shown */
+    isInvalidEntryMessageShown() {
+      return this.invalidEntryMessage != null;
+    },
+  },
+};
+</script>

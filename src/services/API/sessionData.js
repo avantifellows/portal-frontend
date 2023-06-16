3eb87ae91a76dca69cd7ea906e5f3af38fb8a51e
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2023-06-16 10:33:56 +0530

    migrated session API

--- src/services/API/sessionData.js
+++ src/services/API/sessionData.js
@@ -1,4 +1,4 @@
-import { client } from "@/services/API/rootClient.js";
+import { dbClient } from "@/services/API/rootClient.js";
 import { getSessionDataEndpoint } from "@/services/API/endpoints.js";
 
 export default {
@@ -7,12 +7,9 @@ export default {
    * @param {String} sessionId - the ID of the session whose data needs to be retrieved
    */
   getSessionData(sessionId) {
-    const params = {
-      sessionId: sessionId,
-    };
     return new Promise((resolve) => {
-      client
-        .post(getSessionDataEndpoint, JSON.stringify(params))
+      dbClient
+        .get(getSessionDataEndpoint + "/" + sessionId)
         .then((response) => {
           resolve(response.data);
         })

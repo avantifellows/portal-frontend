5930b1b7ed09f0b23545f1ac0327ae019c2c2b9a
Krrupa <25496389+sudheshna-donthineni@users.noreply.github.com>
2023-06-28 10:09:34 +0530

    Update redirectToDestination.js

--- src/services/redirectToDestination.js
+++ src/services/redirectToDestination.js
@@ -15,7 +15,6 @@ export function redirectToDestination(
   redirectTo,
   group
 ) {
-  console.log(redirectId, redirectTo, userId);
   let redirectURL = "";
   let fullURL = "";
   let finalURLQueryParams = "";

3d21316e14db6c1144df4ecc5509cb9e86f53116
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2024-04-04 18:08:11 +0530

    invalid dates for dates less than 1990 and in the future

--- src/components/Datepicker.vue
+++ src/components/Datepicker.vue
@@ -18,6 +18,11 @@
         'mt-[10px] text-sm text-grey italic': true,
       }"
     />
+    <span
+      v-if="isInvalidDateEntryMessageShown"
+      class="text-red text-sm mt-[10px]"
+      >{{ invalidDateEntryMessage }}</span
+    >
   </div>
 </template>
 <script>
@@ -49,6 +54,7 @@ export default {
   data() {
     return {
       date: "",
+      invalidDateEntryMessage: "",
     };
   },
   watch: {
@@ -56,10 +62,25 @@ export default {
      * Emits 'update' event whenever the date value is changed
      */
     date() {
-      this.$emit("update", this.date, this.dbKey);
+      if (
+        new Date(this.date) < new Date("01-01-1990") ||
+        new Date(this.date) > new Date()
+      ) {
+        this.invalidDateEntryMessage = "Please enter a valid date";
+      } else {
+        this.invalidDateEntryMessage = "";
+        this.$emit("update", this.date, this.dbKey);
+      }
     },
   },
   computed: {
+    /**
+     * Checks if the invalid text entry message is shown.
+     * @returns {boolean} True if the invalid text entry message is not null, false otherwise.
+     */
+    isInvalidDateEntryMessageShown() {
+      return this.invalidDateEntryMessage != null;
+    },
     /**
      * Checks if the number entry is valid.
      * @returns {boolean} True if the number entry is valid, or if it is not required. False otherwise.
